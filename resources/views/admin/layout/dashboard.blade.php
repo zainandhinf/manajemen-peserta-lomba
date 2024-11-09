a98efe3d01da4a246fde5e0f10a50a9904dac50c
Ridho Sulistyo <ridhosulistyo1314@gmail.com>
2024-11-09 16:15:17 +0700

    try logout

--- resources/views/admin/layout/dashboard.blade.php
+++ resources/views/admin/layout/dashboard.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
     <h1>Hallo ini dashboard</h1>
+    <form action="/logout" method="POST">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
 @endsection
